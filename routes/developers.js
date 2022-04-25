2020da31fdf75e4701aff05e245d6271654de475
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-25 11:46:58 +0200

    updating

--- routes/developers.js
+++ routes/developers.js
@@ -1,7 +1,11 @@
 require("dotenv").config;
 
+const auth = require("../middleware/auth");
 const express = require("express");
 const Developer = require ('../models/developer')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const getDeveloper = require("../middleware/finders")
 const router = express.Router();
 
 // get
@@ -14,26 +18,44 @@ try{
 }
  });
 
-//  added
- router.post("/", async (req, res, next) => {
-  const { fullname, email, number, portfolio, github } = req.body;
-  let developer;
-(  developer = new Developer({
-        fullname,
-        email,
-        number,
-        portfolio,
-        github
-      
-      }))
-  try {
-    const newDeveloper = await developer.save();
-    res.status(201).json(newDeveloper);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+//  get one
+router.get("/:id", getDeveloper, (req, res, next) => {
+  res.send(res.developer);
 });
 
+
+//  added
+ router.post("/", async (req, res, next) => {
+  const { fullname, email, number,password, portfolio, github } = req.body;
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const developer = new Developer({
+    fullname,
+    email,
+    number,
+    portfolio,
+    github,
+    role,
+    password: hashedPassword,
+  });
+
+  try {
+    const newDeveloper = await developer.save();
+  try {
+    const access_token = jwt.sign(
+      JSON.stringify(newDeveloper),
+      process.env.JWT_SECRET_KEY
+    );
+    res.status(201).json({jwt: access_token, developer});
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+} catch (error) {
+  res.status(400).json({ message: error.message });
+}
+});
+
+// update
 
 
 
